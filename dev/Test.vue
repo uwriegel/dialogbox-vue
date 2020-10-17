2636eca39c3916722185c6219fced2dea610e4b2
Uwe Riegel <uriegel@hotmail.de>
2020-10-17 09:05:24 +0200

    Test.vuec created

---
+++ dev/Test.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="root">
+        Hallo Dialog
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+    data() {
+        return {
+        }
+    },
+})
+</script>
+
+<style>
+:root {
+    --gridsplitter-grip-color: lightgray;
+    --gridsplitter-grip-hover-color: darkgray;
+    --gridsplitter-grip-active-color: #6b6969;
+}
+body {
+    height: 100vh;
+    display:flex;
+    margin: 0px;
+    padding: 0px;
+}
+</style>
+<style scoped>
+</style>
