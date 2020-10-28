d1a040483b69b5b2dfe5cd7bbbfa5819bd5d0858
Uwe Riegel <uriegel@hotmail.de>
2020-10-28 07:18:22 +0100

    install function

---
+++ index.js
@@ -0,0 +1,9 @@
+import DialogBox from "./DialogBox.vue"
+
+export default {
+    install(Vue, options) {
+        // Let's register our component globally
+        // https://vuejs.org/v2/guide/components-registration.html
+        Vue.component("vue-dialog-box", DialogBox)
+    }
+}
