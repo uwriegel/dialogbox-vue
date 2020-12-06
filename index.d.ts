2d42ffa06727492d553b731fb2a1a48d1afd37f3
Uwe Riegel <uriegel@hotmail.de>
2020-12-06 13:11:12 +0100

    Typesafe

--- index.d.ts
+++ index.d.ts
@@ -5,12 +5,20 @@ declare module 'dialogbox-vue' {
 
 import Vue from 'vue'
 
+type DIALOG_CANCEL = 0
+type DIALOG_OK = 1
+type DIALOG_YES = 2
+type DIALOG_NO = 3
+
+type ButtonType = 'ok' | 'cancel' | 'yes' | 'no'
+type DialogResult = DIALOG_OK | DIALOG_CANCEL | DIALOG_YES | DIALOG_NO
+
 export interface DialogOptions {
     ok?: boolean
     cancel?: boolean
     no?: boolean
     yes?: boolean
-    defButton?: string
+    defButton?: ButtonType
     text?: string
     slideLeft?: boolean
     slideRight?: boolean
@@ -18,5 +26,5 @@ export interface DialogOptions {
     textInputValue?: string
 }
 
-export const showDialog: (dialogRef: Vue, dialogOptions: DialogOptions)=>Promise<number>
+export const showDialog: (dialogRef: Vue, dialogOptions: DialogOptions)=>Promise<DialogResult>
 
