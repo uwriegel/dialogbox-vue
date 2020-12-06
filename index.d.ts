ce94bcf36cded05775a8d2900e96e61c61aaa151
Uwe Riegel <uriegel@hotmail.de>
2020-12-06 13:34:10 +0100

    DIALOG_RESULT consts

--- index.d.ts
+++ index.d.ts
@@ -5,13 +5,18 @@ declare module 'dialogbox-vue' {
 
 import Vue from 'vue'
 
-type DIALOG_CANCEL = 0
-type DIALOG_OK = 1
-type DIALOG_YES = 2
-type DIALOG_NO = 3
+type DIALOG_CANCEL_TYPE = 0
+type DIALOG_OK_TYPE = 1
+type DIALOG_YES_TYPE = 2
+type DIALOG_NO_TYPE = 3
+
+export const DIALOG_CANCEL = 0
+export const DIALOG_OK = 1
+export const DIALOG_YES = 2
+export const DIALOG_NO = 3
 
 type ButtonType = 'ok' | 'cancel' | 'yes' | 'no'
-type DialogResult = DIALOG_OK | DIALOG_CANCEL | DIALOG_YES | DIALOG_NO
+type DialogResult = DIALOG_OK_TYPE | DIALOG_CANCEL_TYPE | DIALOG_YES_TYPE | DIALOG_NO_TYPE
 
 export interface DialogOptions {
     ok?: boolean
