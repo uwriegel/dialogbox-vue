228645459800a07a7c54eb2561d7c8030113815c
Uwe Riegel <uriegel@hotmail.de>
2021-01-05 21:38:05 +0100

    const results

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ export default {
 export async function showDialog(dialogRef, dialogOptions) {
     return await dialogRef.show(dialogOptions)
 }
+
+const DIALOG_CANCEL = 0
+const DIALOG_OK = 1
+const DIALOG_YES = 2
+const DIALOG_NO = 3
